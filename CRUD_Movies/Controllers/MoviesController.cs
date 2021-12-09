833fd1d2ae2a390daf42688abb2796260e948c3e
QasemMh <kasem200094@gmail.com>
2021-12-10 01:04:55 +0200

    Add project files.

---
+++ CRUD_Movies/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using CRUD_Movies.Models;
+using CRUD_Movies.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var genres = new MovieFormViewModel
+            {
+                Genres = await _context.Genres.OrderBy(m => m.Name).ToListAsync()
+            };
+            return View(genres);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(MovieFormViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ CRUD_Movies/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(250)]
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public double Rate { get; set; }
+        [Required, MaxLength(2500)]
+        public string StoryLine { get; set; }
+        public string ImagePath { get; set; }
+
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
+
+        public byte GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
